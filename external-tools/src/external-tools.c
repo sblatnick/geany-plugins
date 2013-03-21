1c83bd57d53cca31e37a0c54600f469eed2ac9de
Steven Blatnick <steve8track@yahoo.com>
2013-03-20 19:48:57 -0600

    added execute function

--- external-tools/src/external-tools.c
+++ external-tools/src/external-tools.c
@@ -3,14 +3,14 @@
 #include <string.h>
 #include "tool.c"
 
-GeanyPlugin		 *geany_plugin;
-GeanyData			 *geany_data;
-GeanyFunctions	*geany_functions;
+GeanyPlugin *geany_plugin;
+GeanyData *geany_data;
+GeanyFunctions *geany_functions;
 
 PLUGIN_VERSION_CHECK(211)
 PLUGIN_SET_INFO("External Tools", "Allow external tools to be integrated into many common actions.", "0.1", "Steven Blatnick <steve8track@yahoo.com>");
 
-static gchar *file;
+gchar *path, *file;
 static GKeyFile *config;
 static GtkWidget *tool_menu_item = NULL;
 
@@ -22,12 +22,6 @@ enum
 void dialog()
 {
 	//show the dialog created in plugin_init
-}
-
-void execute_tool(gchar *name)
-{
-	//execute the bash script for the tool, passing it all needed values
-	//and handling the output
 }
 
 static void menu_callback(GtkMenuItem *menuitem, gpointer gdata)
@@ -45,8 +39,10 @@ GtkWidget *plugin_configure(GtkDialog *dialog)
 void plugin_init(GeanyData *data)
 {
 	config = g_key_file_new();
-	file = g_strconcat(geany->app->configdir, G_DIR_SEPARATOR_S, "plugins", G_DIR_SEPARATOR_S,
-		"external-tools", G_DIR_SEPARATOR_S, "external-tools.conf", NULL);
+	
+	path = g_strconcat(geany->app->configdir, G_DIR_SEPARATOR_S,
+		"plugins", G_DIR_SEPARATOR_S, "external-tools", G_DIR_SEPARATOR_S, NULL);
+	file = g_strconcat(path, "external-tools.conf", NULL);
 	g_key_file_load_from_file(config, file, G_KEY_FILE_NONE, NULL);
 
 	//Load existing tools (in groups)
--- external-tools/src/tool.c
+++ external-tools/src/tool.c
@@ -2,9 +2,16 @@
 #include <gdk/gdkkeysyms.h>
 #include <string.h>
 
+extern GeanyPlugin *geany_plugin;
+extern GeanyData *geany_data;
+extern GeanyFunctions *geany_functions;
+
+extern gchar *path, *file;
+
 enum OUTPUT
 {
 	MESSAGE_PANEL = 0,
+	TABLE_PANEL,
 	REPLACE_SELECTED,
 	REPLACE_LINE,
 	REPLACE_WORD,
@@ -20,6 +27,36 @@ typedef struct
 	gboolean menu;
 	gboolean save;
 } Tool;
+
+void execute(Tool *tool)
+{
+	GError *error = NULL;
+	gint status;
+	gchar *std_out, *std_err;
+
+	GString *cmd_str = g_string_new("echo $script");
+	utils_string_replace_all(cmd_str, "$script",
+		g_strconcat(path, G_DIR_SEPARATOR, tool->name, NULL));
+	gchar *cmd = g_string_free(cmd_str, FALSE);
+	cmd = utils_get_locale_from_utf8(cmd);
+
+	if(g_spawn_command_line_sync(
+		cmd,
+		&std_out,
+		&std_err,
+		&status,
+		&error
+	))
+	{
+		printf("std_out: %s", std_out);
+	}
+	else {
+		printf("std_err: %s", std_err);
+		ui_set_statusbar(TRUE, _("ERROR %s: %s (%s)"), cmd, std_err, error->message);
+		g_error_free(error);
+	}
+}
+
 
 Tool* load_tool(gchar *name, GKeyFile *config)
 {
@@ -37,4 +74,3 @@ void load_script(Tool *tool)
 	//load the file of the script into geany
 	//and hide the dialog?
 }
-
