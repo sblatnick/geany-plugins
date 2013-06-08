cd39074e4386df297805401ef5b9c17d1b5dbf6b
Steven Blatnick <steve8track@yahoo.com>
2013-06-07 23:00:43 -0600

    added project directory to external tools

--- external-tools/src/external-tools.c
+++ external-tools/src/external-tools.c
@@ -16,6 +16,7 @@ PLUGIN_VERSION_CHECK(211)
 PLUGIN_SET_INFO("External Tools", "Allow external tools to be integrated into many common actions.", "0.1", "Steven Blatnick <steve8track@yahoo.com>");
 
 gchar *path, *conf, *tools;
+const char *home;
 
 static GtkWidget *tool_menu_item = NULL;
 
@@ -26,6 +27,10 @@ static void menu_callback(GtkMenuItem *menuitem, gpointer gdata)
 
 void plugin_init(GeanyData *data)
 {
+	home = g_getenv("HOME");
+	if (!home) {
+		home = g_get_home_dir();
+	}
 	path = g_build_path(G_DIR_SEPARATOR_S, geany_data->app->configdir, "plugins", "external-tools", NULL);
 	conf = g_build_path(G_DIR_SEPARATOR_S, path, "external-tools.conf", NULL);
 	tools = g_build_path(G_DIR_SEPARATOR_S, path, "tools", NULL);
