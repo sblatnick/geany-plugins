d50bf91d721ad14d61131bd956d2638f994ea5ad
Steven Blatnick <steve8track@yahoo.com>
2014-12-16 14:38:25 -0700

    hide-menu fix from merging in upstream

--- hide-menu/src/hide-menu.c
+++ hide-menu/src/hide-menu.c
@@ -54,11 +54,11 @@ static gboolean on_unlock(GtkWidget *widget, GdkEvent *event, gpointer user_data
 
 void plugin_init(GeanyData *data)
 {
-	menubar = ui_lookup_widget(main_widgets.window, "hbox_menubar");
-	menu = ui_lookup_widget(main_widgets.window, "menubar1");
+	menubar = ui_lookup_widget(geany->main_widgets->window, "hbox_menubar");
+	menu = ui_lookup_widget(geany->main_widgets->window, "menubar1");
 	gtk_widget_add_events(menu, GDK_POINTER_MOTION_MASK | GDK_LEAVE_NOTIFY_MASK | GDK_BUTTON_PRESS_MASK);
 	//in case the toolbar is on the menubar, let's include it (buggy):
-	gtk_widget_add_events(main_widgets.toolbar, GDK_POINTER_MOTION_MASK | GDK_LEAVE_NOTIFY_MASK);
+	gtk_widget_add_events(geany->main_widgets->toolbar, GDK_POINTER_MOTION_MASK | GDK_LEAVE_NOTIFY_MASK);
 	gtk_widget_set_size_request(menubar, -1, height);
 	inMenuHandler = g_signal_connect(menubar, "motion-notify-event", G_CALLBACK(on_in), NULL);
 	activateMenuHandler = g_signal_connect(menu, "button-press-event", G_CALLBACK(on_lock), NULL);
